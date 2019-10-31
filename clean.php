23a8b7f4e919066cb0bb80f350a7de9e1494d361
aehandasa <42756622+aehandasa@users.noreply.github.com>
2019-10-31 21:50:41 +0200

    Update clean.php

--- clean.php
+++ clean.php
@@ -1,9 +1,9 @@
 <?php
 
 function clean($inpt){
-			$chr  = array("/", "*", ".", "'", "-", ";", ":", ",", "=");
-			$otpt = str_replace($chr, "", $inpt);
-			return $otpt;
+	$chr  = array("/", "*", ".", "'", "-", ";", ":", ",", "=");
+	$otpt = str_replace($chr, "", $inpt);
+	return $otpt;
 }
 
 ?>
